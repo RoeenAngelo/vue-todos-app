4e5da366eedaccf52f1bfb584fb21f389c8453ce
Roeen <roeenacal@gmail.com>
2023-06-10 15:59:52 -0700

    Add routes to header logo

--- src/router/index.js
+++ src/router/index.js
@@ -15,9 +15,8 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      // this is best practice
+      // this is best practice!
       component: () => import('../views/AboutView.vue')
-      // component: AboutView
     },
     {
       path: '/contact',
