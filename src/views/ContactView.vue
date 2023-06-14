32013d2c0be41772bbbe1d0b442f67bc0e7f4d45
Roeen <roeenacal@gmail.com>
2023-06-13 21:18:15 -0700

    fix about page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="about">
+    <div class="contact">
       <h1>Contact Details</h1>
     </div>
   </template>
   
   <style>
   @media (min-width: 1024px) {
-    .about {
+    .contact {
       min-height: 100vh;
       display: flex;
       align-items: center;
